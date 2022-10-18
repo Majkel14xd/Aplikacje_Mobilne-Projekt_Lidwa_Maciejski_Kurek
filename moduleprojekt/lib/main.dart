23ee3b241e347e744aff8f12dfa06de3c439f020
KacperKurek2109 <kacperkurek2109@gmail.com>
2022-10-19 01:41:54 +0200

    Kacper Kurek

--- moduleprojekt/lib/main.dart
+++ moduleprojekt/lib/main.dart
@@ -22,9 +22,9 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.green,
+        primarySwatch: Colors.teal,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Ustawienia'),
     );
   }
 }
@@ -48,68 +48,226 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
+      backgroundColor: Colors.teal.shade400,
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: Padding(
+          padding: const EdgeInsets.only(left:10,top:10,right: 10),
+          child: Column(
+            children: <Widget>[
+              const SizedBox(height: 15),
+              _firstWidget(),
+              const SizedBox(height: 15),
+              _secondWidget(),
+              const SizedBox(height: 15),
+              _thirdWidget(),
+              const SizedBox(height: 15),
+              _fourthWidget(),
+              const SizedBox(height: 15),
+              _fifthWidget(),
+              Image.network('https://cdn.pixabay.com/photo/2019/03/11/21/18/city-4049526_1280.png'),
+
+            ],
+          )),
     );
   }
-}
+
+  Widget _firstWidget() {
+    return InkWell(
+        onTap: () {},
+        child: Card(
+          shape: BeveledRectangleBorder(
+            borderRadius: BorderRadius.circular(5.0),
+          ),
+          elevation: 2,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Expanded(
+                  flex: 1,
+                  child: Container(
+                    height: 55,
+                    width: 30,
+                    decoration: myBoxDecoration(),
+                    child: const Icon(
+                      Icons.account_circle,
+                      color: Colors.white,
+                    ),
+                  )),
+              const Expanded(
+                  flex: 3,
+                  child: Padding(
+                      padding: EdgeInsets.only(left: 15),
+                      child: Text(
+                        'DANE UŻYTKOWNIKA',
+                        style: TextStyle(
+                          fontSize: 25,
+                        ),
+                      ))),
+            ],
+          ),
+        ));
+  }
+
+  Widget _secondWidget() {
+    return InkWell(
+        onTap: () {},
+        child: Card(
+          shape: BeveledRectangleBorder(
+            borderRadius: BorderRadius.circular(5.0),
+          ),
+          elevation: 2,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Expanded(
+                  flex: 1,
+                  child: Container(
+                    decoration: myBoxDecoration(),
+                    height: 55,
+                    width: 30,
+                    child: const Icon(
+                      Icons.security_sharp,
+                      color: Colors.white,
+                    ),
+                  )),
+              const Expanded(
+                  flex: 3,
+                  child: Padding(
+                      padding: EdgeInsets.only(left: 15),
+                      child: Text(
+                        'BEZPIECZEŃSTWO',
+                        style: TextStyle(
+                          fontSize: 25,
+                        ),
+                      ))),
+            ],
+          ),
+        ));
+  }
+  Widget _thirdWidget() {
+    return InkWell(
+        onTap: () {},
+        child: Card(
+          shape: BeveledRectangleBorder(
+            borderRadius: BorderRadius.circular(5.0),
+          ),
+          elevation: 2,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Expanded(
+                  flex: 1,
+                  child: Container(
+                    decoration: myBoxDecoration(),
+                    height: 55,
+                    width: 30,
+                    child: const Icon(
+                      Icons.auto_awesome_mosaic_outlined,
+                      color: Colors.white,
+                    ),
+                  )),
+              const Expanded(
+                  flex: 3,
+                  child: Padding(
+                      padding: EdgeInsets.only(left: 15),
+                      child: Text(
+                        'OPCJE MOTYWU',
+                        style: TextStyle(
+                          fontSize: 25,
+                        ),
+                      ))),
+            ],
+          ),
+        ));
+  }
+  Widget _fourthWidget() {
+    return InkWell(
+        onTap: () {},
+        child: Card(
+          shape: BeveledRectangleBorder(
+            borderRadius: BorderRadius.circular(5.0),
+          ),
+          elevation: 2,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Expanded(
+                  flex: 1,
+                  child: Container(
+                    decoration: myBoxDecoration(),
+                    height: 55,
+                    width: 30,
+                    child: const Icon(
+                      Icons.swap_horizontal_circle,
+                      color: Colors.white,
+                    ),
+                  )),
+              const Expanded(
+                  flex: 3,
+                  child: Padding(
+                      padding: EdgeInsets.only(left: 15),
+                      child: Text(
+                        'SORTUJ BILETY',
+                        style: TextStyle(
+                          fontSize: 25,
+                        ),
+                      ))),
+            ],
+          ),
+        ));
+  }
+  Widget _fifthWidget() {
+    return InkWell(
+        onTap: () {},
+        child: Card(
+          shape: BeveledRectangleBorder(
+            borderRadius: BorderRadius.circular(5.0),
+          ),
+          elevation: 2,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+
+            children: <Widget>[
+              Expanded(
+                  flex: 1,
+                  child: Container(
+                    decoration: myBoxDecoration(),
+                    height: 55,
+                    width: 30,
+                    child: const Icon(
+                      Icons.keyboard_backspace_sharp,
+                      color: Colors.white,
+                    ),
+                  )),
+              const Expanded(
+                  flex: 3,
+                  child: Padding(
+                      padding: EdgeInsets.only(left: 15),
+                      child: Text(
+                        'COFNIJ',
+                        style: TextStyle(
+                          fontSize: 25,
+                        ),
+                      ))),
+            ],
+          ),
+        ));
+  }
+
+  BoxDecoration myBoxDecoration() {
+    return const BoxDecoration(
+      color: Colors.teal,
+      borderRadius: BorderRadius.all(
+          Radius.circular(5.0) //         <--- border radius here
+      ),
+    );
+  }
+  }
+
