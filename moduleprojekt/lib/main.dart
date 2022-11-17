0026f05d640854689da7d8b24143d4429eaaea3b
KacperKurek2109 <kacperkurek2109@gmail.com>
2022-11-18 00:13:21 +0100

    Wnętrze podstrony w Ustawieniach Profilu - kliknięcie w zdjecie profilowe powoduje przejscie do edycji danych profilu , przycisk save jak narazie jedynie powraca do poprzedniego okna bez zapisu wprowadzonych danych w pliku znajduje sie zadeklarowany widget typu rozwijane menu kontekstowe z wyborem ulg ktore narazie nie zostalo zastosowane (zostanie podstawione pod obecny obszar tekstowy)

--- moduleprojekt/lib/main.dart
+++ moduleprojekt/lib/main.dart
@@ -7,21 +7,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.teal,
       ),
       home: const Ustawienia(title: 'Ustawienia'),
@@ -32,17 +22,7 @@ class MyApp extends StatelessWidget {
 class Ustawienia extends StatefulWidget {
   const Ustawienia({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
-
   @override
   State<Ustawienia> createState() => _Ustawienia();
 }
@@ -53,8 +33,6 @@ class _Ustawienia extends State<Ustawienia> {
     return Scaffold(
       backgroundColor: Colors.teal.shade400,
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Padding(
@@ -153,7 +131,7 @@ class Przyciski extends StatelessWidget {
                     decoration: BoxDecoration(
                       color: Colors.teal,
                       borderRadius: BorderRadius.all(
-                          Radius.circular(5.0) //         <--- border radius here
+                          Radius.circular(5.0)
                       ),
                     ),
                     child:Icon(
